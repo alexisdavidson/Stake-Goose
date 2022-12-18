86f1633aec3d653087de27b91a40391c3efe462b
Alexis Davidson <alexisdavidson00@gmail.com>
2022-12-18 18:49:32 +0700

    Deploy Goerli

---
+++ src/backend/scripts/argumentsNftStaker.js
@@ -0,0 +1,4 @@
+module.exports = [
+    "0x46Ce726522BEc0c410D8896ba16A330B416f1cf0", // nft goose address
+    "0x0000000000000000000000000000000000000000" // wolf address
+];
--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -1,3 +1,5 @@
+const TokenEggAbi = require('../../frontend/contractsData/Token_Egg.json')
+
 const fromWei = (num) => ethers.utils.formatEther(num)
 
 async function main() {
@@ -28,27 +30,46 @@ async function main() {
 
   // Phase 2 deployment code
   // const plantingAddress = "0x2792a863fcD0e2ad62272f6aD6E8746DBC527aF6" // Goerli
-  const plantingAddress = "0xEBfCF2708F4DA3E07Eb55c8D599e97fEE4c2ACF5" // Mainnet
+  // const plantingAddress = "0xEBfCF2708F4DA3E07Eb55c8D599e97fEE4c2ACF5" // Mainnet
 
-  const NFT_Goose = await ethers.getContractFactory("NFT_Goose");
-  const Token_Egg = await ethers.getContractFactory("Token_Egg");
-  const Castle = await ethers.getContractFactory("Castle");
+  // const NFT_Goose = await ethers.getContractFactory("NFT_Goose");
+  // const Token_Egg = await ethers.getContractFactory("Token_Egg");
+  // const Castle = await ethers.getContractFactory("Castle");
 
-  const nftGoose = await NFT_Goose.deploy();
-  console.log("nftGoose contract address", nftGoose.address)
-  saveFrontendFiles(nftGoose, "NFT_Goose");
+  // const nftGoose = await NFT_Goose.deploy();
+  // console.log("nftGoose contract address", nftGoose.address)
+  // saveFrontendFiles(nftGoose, "NFT_Goose");
 
-  const castle = await Castle.deploy(plantingAddress, nftGoose.address);
-  console.log("castle contract address", castle.address)
-  saveFrontendFiles(castle, "Castle");
+  // const castle = await Castle.deploy(plantingAddress, nftGoose.address);
+  // console.log("castle contract address", castle.address)
+  // saveFrontendFiles(castle, "Castle");
 
-  const tokenEgg = await Token_Egg.deploy(castle.address, deployer.address);
-  console.log("tokenEgg contract address", tokenEgg.address)
-  saveFrontendFiles(tokenEgg, "Token_Egg");
+  // const tokenEgg = await Token_Egg.deploy(castle.address, deployer.address);
+  // console.log("tokenEgg contract address", tokenEgg.address)
+  // saveFrontendFiles(tokenEgg, "Token_Egg");
 
-  await nftGoose.setCastleAddress(castle.address);
-  await castle.setTokenAddress(tokenEgg.address);
-  console.log("Set address functions called")
+  // await nftGoose.setCastleAddress(castle.address);
+  // await castle.setTokenAddress(tokenEgg.address);
+  // console.log("Set address functions called")
+
+  // Phase 3 deployment code
+  let wolfAddress = "0x0000000000000000000000000000000000000000" // Replace with Wolf address
+  let nftGooseAddress = "0x46Ce726522BEc0c410D8896ba16A330B416f1cf0" // Goerli
+  let tokenEggAddress = "0xb59691e131c2e5cCf55CC1aC9a365b96E25b4787" // Goerli
+
+  const NFTStaker =  await ethers.getContractFactory("NFTStaker");
+  const nftStaker = await NFTStaker.deploy(nftGooseAddress, wolfAddress);
+  console.log("nftStaker contract address", nftStaker.address)
+  saveFrontendFiles(nftStaker, "NFTStaker");
+
+  await nftStaker.setTokenAddress(tokenEggAddress);
+  console.log("nftStaker setTokenAddress functions called")
+  
+  // Optional: Transfer tokens from team wallet to staker address
+  const tokenEgg = new ethers.Contract(tokenEggAddress, TokenEggAbi.abi)
+  let stakerTokenBalance = 10_000;
+  await tokenEgg.connect(deployer).transfer(nftStaker.address, stakerTokenBalance);
+  console.log("tokens transfered to nft staker")
 }
 
 function saveFrontendFiles(contract, name) {
